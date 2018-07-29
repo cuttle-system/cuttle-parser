5545fedbda226d1a9257305a01e3c322f346f91f
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-07-29 14:57:09 +0300

    refactor and move to new test framework

--- src/call_tree.hpp
+++ src/call_tree.hpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 namespace cuttle {
-	using tree_src_t = std::vector<std::vector<int> >;
+	using tree_src_t = std::vector<std::vector<unsigned int> >;
 	struct call_tree_t {
         tree_src_t src;
     };
