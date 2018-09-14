0d70fa5c3c82381830d323b4e1defaa9db8146ac
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-09-14 21:30:46 +0300

    add call tree types

--- src/call_tree.hpp
+++ src/call_tree.hpp
@@ -4,9 +4,13 @@
 #include <limits>
 
 namespace cuttle {
-    const unsigned int CALL_TREE_SRC_UNDEFINED = std::numeric_limits<unsigned int>::max();
-	using tree_src_t = std::vector<std::vector<unsigned int> >;
-	struct call_tree_t {
+	using tree_src_element_t = unsigned int;
+    using tree_src_elements_t = std::vector<tree_src_element_t>;
+    using tree_src_t = std::vector<tree_src_elements_t>;
+
+    const tree_src_element_t CALL_TREE_SRC_UNDEFINED = std::numeric_limits<tree_src_element_t>::max();
+
+    struct call_tree_t {
         tree_src_t src;
     };
 }
